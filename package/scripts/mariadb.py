b9e48e0080a548d435b32ce07f43cc8be3fbc0e4
xuzhe <geniuszhe@163.com>
2016-07-29 17:07:58 +0800

    add password

--- package/scripts/mariadb.py
+++ package/scripts/mariadb.py
@@ -1,9 +1,11 @@
 import os
+import base64
 from time import sleep
 from resource_management import *
 
 class MariadbMaster(Script):
     mariadb_packages=['MariaDB-Galera-server','MariaDB-client','galera']
+    db_pass_file='/etc/db_info.cnf'
     
     def install(self, env):
         #delete mysql-server
@@ -19,31 +21,54 @@ class MariadbMaster(Script):
         #configure cluster
         Execute('service mysql start')
         sleep(10)
-        
-        Execute("/usr/bin/mysqladmin -u root password 'dbpass'",ignore_failures=True)
+        db_password = params.db_password
+        cmd = format("/usr/bin/mysqladmin -u root password '{db_password}'")
+        Execute(cmd,ignore_failures=True)
         
         #init db
-        Execute('find '+params.service_packagedir+' -iname "*.sh" | xargs chmod +x')
-        service_packagedir = params.service_packagedir
-        
-        cmd = format("{service_packagedir}/scripts/init_db.sh")
-        Execute('echo "Running ' + cmd + '" as root')
-        Execute(cmd,ignore_failures=True)  
+        self.initdb(env)
         
         Execute('service mysql stop')
         sleep(5)
-        
+               
+
+    def configure(self, env):       
+        print 'configure mariadb'       
+        import params
+        db_dir = params.db_dir
+        if os.path.exists(db_dir):
+            print "Dir exists"
+        else:
+            Execute(format('mkdir -p {db_dir}'),logoutput=True)
+            Execute(format('chown -R mysql:mysql {db_dir}'),logoutput=True)
+            Execute(format('mysql_install_db --user=mysql --ldata={db_dir}'),logoutput=True)
+            
         env.set_params(params)       
         File("/etc/my.cnf.d/server.cnf",
              content=Template("server.cnf.j2"),
              mode=0644
-            )        
-
-    def configure(self, env):       
-        print 'configure mariadb'
+            )
+        db_password = params.db_password
+        
+        file_object = open(self.db_pass_file)
+        try:
+            file_content = file_object.read( )
+            pre_pass= base64.decodestring(file_content)
+            print pre_pass
+            if db_password !=pre_pass:
+                Execute('service mysql start')
+                sleep(10)
+                cmd = format("/usr/bin/mysqladmin -u root -p{pre_pass} password '{db_password}'")
+                Execute(cmd)
+                self.initdb(env)
+                Execute('service mysql stop')
+        finally:
+            file_object.close( )
+                
         
 
     def start(self, env):
+        self.configure(env)
         import params
         if params.mariadb_current_host == params.mariadb_hosts[0]:
             Execute('/etc/init.d/mysql start --wsrep-new-cluster')
@@ -62,6 +87,29 @@ class MariadbMaster(Script):
 
     def status(self, env):
        Execute('service mysql status')
+    
+    def initdb(self,env):
+        import params;
+        env.set_params(params)  
+        service_packagedir = params.service_packagedir
+        init_lib_path = service_packagedir + '/scripts/init_lib.sh'
+        File(init_lib_path,
+             content=Template("init_lib.sh.j2"),
+             mode=0777
+            )
+        cmd = format("{service_packagedir}/scripts/init_lib.sh")
+        Execute('echo "Running ' + cmd + '" as root')
+        Execute(cmd)
+        db_pass_file = self.db_pass_file
+        cmd = format("rm -rf {db_pass_file}")
+        Execute('echo "Running ' + cmd + '" as root')
+        Execute(cmd)
+        
+        db_password=base64.encodestring(params.db_password)
+        File(self.db_pass_file,
+             content=db_password,
+             mode=0644
+            ) 
 
 if __name__ == "__main__":
     MariadbMaster().execute()
--- package/scripts/params.py
+++ package/scripts/params.py
@@ -7,7 +7,8 @@ from resource_management.libraries.functions.default import default
 # server configurations
 config = Script.get_config()
 
-db_dir='/var/lib/mysql'
+db_dir=default('configurations/mariadb/db_path', '/var/lib/mysql')
+db_password=default('configurations/mariadb/db_password', 'dbpass')
 #e.g. /var/lib/ambari-agent/cache/stacks/HDP/2.3/services/SOLR/package
 service_packagedir = os.path.realpath(__file__).split('/scripts')[0]
 mariadb_hosts = config['clusterHostInfo']['mariadb_hosts']
